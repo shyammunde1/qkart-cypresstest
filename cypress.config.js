a93cfe492a401ed4baee679ea64f5128c582dc9f
samtech <shyammunde1@gmail.com>
2024-04-16 15:50:25 +0530

    cypress project id added

--- cypress.config.js
+++ cypress.config.js
@@ -6,7 +6,7 @@ module.exports = defineConfig({
     QKART_FRONTEND: "https://qkart-frontend-six-flame.vercel.app",
     QKART_BACKEND: "https://qkart-frontend1-jfw4.onrender.com/api/v1",
   },
-
+  projectId: "d2pyp7",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
