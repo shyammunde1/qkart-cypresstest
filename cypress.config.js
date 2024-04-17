0afd87f05f4290aaa70e5de56cf5ac59200a4930
samtech <shyammunde1@gmail.com>
2024-04-17 15:58:10 +0530

    report genrater added mocha and allure report

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,30 @@
 const { defineConfig } = require("cypress");
 
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here
+  require("cypress-mochawesome-reporter/plugin")(on);
+  return config;
+}
+
 module.exports = defineConfig({
-  defaultCommandTimeout: 5000,
+  reporter: "cypress-mochawesome-reporter",
   env: {
     QKART_FRONTEND: "https://qkart-frontend-six-flame.vercel.app",
     QKART_BACKEND: "https://qkart-frontend1-jfw4.onrender.com/api/v1",
   },
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: "Qkart Test Report",
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
   projectId: "d2pyp7",
+
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    setupNodeEvents,
     specPattern: "cypress/integration/qkart/*.js",
+    experimentalModifyObstructiveThirdPartyCode: true,
+    defaultCommandTimeout: 10000,
   },
 });
